909fe9e12f9a2356e04a75d6c10902c66ae31741
Benoit Simon <benoit@sxnths.com>
2022-11-15 20:53:27 +0100

    - started using OR-Tools - rolled back OR-Tools version to be compatible with osx 10.9

--- cloudrun/core.py
+++ cloudrun/core.py
@@ -9,6 +9,8 @@ import copy
 import math , random
 from io import BytesIO
 
+# bin packing
+from ortools.linear_solver import pywraplp
 
 random.seed()
 
@@ -549,7 +551,100 @@ class CloudRunProvider(ABC):
         # knapsack / 2d packing / bin packing ...
         # https://developers.google.com/optimization/bin/multiple_knapsack
         elif assignation=='multi_knapsack':
-            pass
+            
+            self._multiple_knapsack_assignation()
+
+    def _multiple_knapsack_assignation(self):
+
+        # FILL UP the data
+
+        data = {}
+        data['weights'] = []
+        data['values']  = []
+        for job in self._jobs:
+            cpu_req = job.get_config('cpu_reqs')
+            if cpu_req is None:
+                cpu_req = 1
+            
+            weight = cpu_req
+            value  = cpu_req
+            data['weights'].append(weight)
+            data['values'].append(value)
+
+        assert len(data['weights']) == len(data['values'])
+        data['num_items'] = len(data['weights'])
+        data['all_items'] = range(data['num_items'])
+
+        data['bin_capacities'] = []
+        for instance in self._instances:
+            cpus = instance.get_config('cpus')
+            if cpus is None:
+                cpus = 1 #TODO: use the spec from the type ....
+
+        data['num_bins'] = len(data['bin_capacities'])
+        data['all_bins'] = range(data['num_bins'])
+
+        # DECLARE the Solver
+
+        solver = pywraplp.Solver.CreateSolver('SCIP')
+        if solver is None:
+            print('SCIP solver unavailable.')
+            return 
+
+        # CREATE the variables
+
+        # x[i, b] = 1 if item i is packed in bin b.
+        x = {}
+        for i in data['all_items']:
+            for b in data['all_bins']:
+                x[i, b] = solver.BoolVar(f'x_{i}_{b}')
+
+        # DEFINE the constraints
+
+        # Each item is assigned to at most one bin.
+        for i in data['all_items']:
+            solver.Add(sum(x[i, b] for b in data['all_bins']) <= 1)
+
+        # The amount packed in each bin cannot exceed its capacity.
+        for b in data['all_bins']:
+            solver.Add(
+                sum(x[i, b] * data['weights'][i]
+                    for i in data['all_items']) <= data['bin_capacities'][b])    
+
+        # DEFINE the objective
+
+        # Maximize total value of packed items.
+        objective = solver.Objective()
+        for i in data['all_items']:
+            for b in data['all_bins']:
+                objective.SetCoefficient(x[i, b], data['values'][i])
+        objective.SetMaximization()    
+
+        status = solver.Solve()
+
+        # PRINT the solution 
+
+        if status == pywraplp.Solver.OPTIMAL:
+            print(f'Total packed value: {objective.Value()}')
+            total_weight = 0
+            for b in data['all_bins']:
+                print(f'Bin {b}')
+                bin_weight = 0
+                bin_value = 0
+                for i in data['all_items']:
+                    if x[i, b].solution_value() > 0:
+                        print(
+                            f"Item {i} weight: {data['weights'][i]} value: {data['values'][i]}"
+                        )
+                        bin_weight += data['weights'][i]
+                        bin_value += data['values'][i]
+                print(f'Packed bin weight: {bin_weight}')
+                print(f'Packed bin value: {bin_value}\n')
+                total_weight += bin_weight
+            print(f'Total packed weight: {total_weight}')
+        else:
+            print('The problem does not have an optimal solution.')                
+               
 
     async def _start_and_wait_for_instance(self,instance):
         # CHECK EVERY TIME !
